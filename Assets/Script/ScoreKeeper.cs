5bc6c0b2309c56a09fee430e3ce7cbba2819befc
StavroulaKoutsikou <melenevoula10@gmail.com>
2024-03-22 21:58:08 +0200

    first commit
    first commit

---
+++ Assets/Script/ScoreKeeper.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    int correctAnswers = 0;
+    int questionsSeen = 0;
+
+    public int GetCorrectAnswers()
+    {
+        return correctAnswers;
+    }
+
+    public void IncreamentCorrectAnswers()
+    {
+        correctAnswers++;
+    }
+
+    public int GetQuestionSeen()
+    {
+        return questionsSeen;
+    }
+
+    public void IncreamentQuestionsSeen()
+    {
+        questionsSeen++;
+    }
+
+    public int CalculateScore()
+    {
+        return Mathf.RoundToInt(correctAnswers / (float)questionsSeen * 100);
+    }
+}
